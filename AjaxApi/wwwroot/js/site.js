61d7cad84f4dba410a423c2f8298fc6e150a6bc4
Minh Viet Pham <47558555+Izay0i@users.noreply.github.com>
2022-11-19 07:47:04 +0700

    Initial commit

---
+++ AjaxApi/wwwroot/js/site.js
@@ -0,0 +1,120 @@
+﻿const uri = 'api/todoitems';
+let todos = [];
+
+function getItems() {
+    $.ajax({
+        url: uri,
+        type: 'GET',
+        dataType: 'json',
+        success: (data => _displayItems(data))
+    });
+}
+
+function addItem() {
+    const addNameTextbox = document.getElementById('add-name');
+
+    const item = {
+        isComplete: false,
+        name: addNameTextbox.value.trim()
+    };
+
+    $.ajax({
+        url: uri,
+        type: 'POST',
+        data: JSON.stringify(item),
+        contentType: 'application/json',
+        success: (() => {
+            getItems();
+            addNameTextbox.value = '';
+        })
+    });
+}
+
+function deleteItem(id) {
+    $.ajax({
+        url: `${uri}/${id}`,
+        type: 'DELETE',
+        success: (() => getItems())
+    });
+}
+
+function displayEditForm(id) {
+    const item = todos.find(item => item.id === id);
+
+    document.getElementById('edit-name').value = item.name;
+    document.getElementById('edit-id').value = item.id;
+    document.getElementById('edit-isComplete').checked = item.isComplete;
+    document.getElementById('editForm').style.display = 'block';
+}
+
+function updateItem() {
+    const itemId = document.getElementById('edit-id').value;
+    const item = {
+        id: parseInt(itemId, 10),
+        isComplete: document.getElementById('edit-isComplete').checked,
+        name: document.getElementById('edit-name').value.trim()
+    };
+
+    $.ajax({
+        url: `${uri}/${itemId}`,
+        type: 'PUT',
+        data: JSON.stringify(item),
+        contentType: 'application/json',
+        success: (() => getItems())
+    });
+
+    closeInput();
+
+    return false;
+}
+
+function closeInput() {
+    document.getElementById('editForm').style.display = 'none';
+}
+
+function _displayCount(itemCount) {
+    const name = (itemCount === 1) ? 'to-do' : 'to-dos';
+
+    document.getElementById('counter').innerText = `${itemCount} ${name}`;
+}
+
+function _displayItems(data) {
+    const tBody = document.getElementById('todos');
+    tBody.innerHTML = '';
+
+    _displayCount(data.length);
+
+    const button = document.createElement('button');
+
+    data.forEach(item => {
+        let isCompleteCheckbox = document.createElement('input');
+        isCompleteCheckbox.type = 'checkbox';
+        isCompleteCheckbox.disabled = true;
+        isCompleteCheckbox.checked = item.isComplete;
+
+        let editButton = button.cloneNode(false);
+        editButton.innerText = 'Edit';
+        editButton.setAttribute('onclick', `displayEditForm(${item.id})`);
+
+        let deleteButton = button.cloneNode(false);
+        deleteButton.innerText = 'Delete';
+        deleteButton.setAttribute('onclick', `deleteItem(${item.id})`);
+
+        let tr = tBody.insertRow();
+
+        let td1 = tr.insertCell(0);
+        td1.appendChild(isCompleteCheckbox);
+
+        let td2 = tr.insertCell(1);
+        let textNode = document.createTextNode(item.name);
+        td2.appendChild(textNode);
+
+        let td3 = tr.insertCell(2);
+        td3.appendChild(editButton);
+
+        let td4 = tr.insertCell(3);
+        td4.appendChild(deleteButton);
+    });
+
+    todos = data;
+}
